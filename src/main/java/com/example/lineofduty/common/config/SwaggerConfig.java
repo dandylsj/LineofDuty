f00f281b003c7fbfc8ab1692cc7efb42d3ebfe50
lsjya <lsjyahoo@gmail.com>
2026-05-16 00:44:17 +0900

    fix: Swagger 외부 접속 오류 수정 - 포트 누락

--- src/main/java/com/example/lineofduty/common/config/SwaggerConfig.java
+++ src/main/java/com/example/lineofduty/common/config/SwaggerConfig.java
@@ -1,15 +1,17 @@
 package com.example.lineofduty.common.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
-@OpenAPIDefinition(
-        security = @SecurityRequirement(name = "bearerAuth")
-)
 @SecurityScheme(
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
@@ -17,4 +19,22 @@ import org.springframework.context.annotation.Configuration;
         bearerFormat = "JWT"
 )
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Line of Duty API")
+                        .version("1.0.0")
+                        .description("Line of Duty 백엔드 API 문서"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .servers(List.of(
+                        new Server()
+                                .url("https://lsjyahoo.synology.me:8443")
+                                .description("NAS 운영 서버"),
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("로컬 개발 서버")
+                ));
+    }
 }
